4b481f07b36e54ef6a4b0edb7123d44b2f050dfc
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2022-06-07 06:18:17 -0300

    tela
    Tela e espaço nave

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,11 +1,17 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 def run_game():
     #inicializa o jogo e cria um objeto na tela
     pygame.init()
-    screen = pygame.display.set_mode((1100, 650))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion - by Rodrigo Abreu")
+
+    #Cria uma espaçonave
+    ship = Ship(screen)
 
     # Define uma cor de fundo
     bg_color = (32,124,229,1)
@@ -18,7 +24,8 @@ def run_game():
                 sys.exit()
 
         #Redesenha a tela a cada passagem pelo laço
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         #deixa a tela mais recente visivel
         pygame.display.flip()
